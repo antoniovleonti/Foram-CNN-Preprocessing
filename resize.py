fb7d08338fbe17bdaa0e7a8fa2db82744c22b7ad
Antonio V. Leonti <aleonti@umassd.edu>
2020-01-07 18:40:42 -0500

    Update resize.py
    removed process_dir(), which was only really useful for one use case (the first one i had)
    
    cleaned up code a bit

--- resize.py
+++ resize.py
@@ -9,71 +9,61 @@ import cv2
 import numpy
 
 
-def main():# example usage
-    root = "/Users/antoniovleonti/Desktop/Research/"
-    dirs=( # paths to images (relative to root/<src|data>/) & threshs
-        ("validation/amphistegina/",.01), ("training/amphistegina/",.01),
-        ("validation/glob/",.05),         ("training/glob/",.05)
+def main(): # preprocess a training & validation datset from a single source
+    root = "/Users/antoniovleonti/Desktop/Research/data/"
+    classes = ( # paths to images (relative to root/<src|data>/) & threshs
+        ("amp_radiarta/", .01), ("glob_menardii/",.01), ("glob_ruber/", .05),
+        ("nglob_dutertrei/",.05), ("tril_sacculifer/", .01)
     )
+    exts = ("jpg", "jpeg", "png", "tif")
     l = 64 # height and width
 
-    for d, t in dirs:
-        # process, copy to another directory
-        print("processing: "+root+"src/"+d)
-        processdir(
-            srcdir = root+"src/"+d,
-            dstdir = root+"data/"+d,
-            height = l, thresh = t
-        )
-
-
-def process_dir(srcdir, height, dstdir = None, width = None, thresh = 0):
-    """processdir() iterates through all images in srcdir/, 'frames' the
-    contents (crop()'s in a square around it), then resizes the image to
-    height x width before saving to dstdir/ (or back to srcdir/)
-    """
-    #default values of optional parameters
-    if dstdir == None: dstdir = srcdir
-    if width == None: width = height
-
-    #get a list of all files in dir; iterate through them
-    for name in listdir(srcdir):
-        #check extension
-        if name.rpartition('.')[-1].lower() in ("jpg", "jpeg", "png", "tif"):
-            #crop
-            img = crop(src = cv2.imread(srcdir+name,0), thresh = thresh)
-            #resize & save
-            cv2.imwrite(dstdir+name, cv2.resize(img, (width,height)))
+    # for each species
+    for c, t in classes:
+        # get the names of each enclosed file
+        for name in listdir(root+"src/"+c):
+            # check extension
+            if name.rpartition('.')[-1].lower() in exts:
+                # determine output path
+                path = "train/" if name.rpartition('_')[-1][0]!='5' else "vali/"
+                # open & crop image
+                img = crop(src = cv2.imread(root+"src/"+c+name,0), thresh = t)
+                # resize & save
+                cv2.imwrite(
+                    root + path + c + name,
+                    cv2.resize(img, (l,l))
+                )
 
 
 def crop(src, thresh = 0):
-    """crop() removes the empty space around an image's contents. It draws
-    bounding boxes around
+    """crop() removes the empty space around an image's contents. For an object
+    to expand the crop, its hypotenuse must be as big as the hypotenuse of src
+    times thresh (if hypot(src)=100 and thresh=.05, hypot(obj) must be >= 5)
     """
-    #threshold
-    thr = cv2.threshold(src, 0, 255, cv2.THRESH_OTSU)[1]
-    #findcontours
-    ctr = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
+    #threshold, then find contours
+    ctr = cv2.threshold(src, 0, 255, cv2.THRESH_OTSU)[1]
+    ctr = cv2.findContours(ctr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1]
 
-    xmin, ymin = (max(thr.shape[:2]), )*2 #initialize to opposite edges of image
+    xmin, ymin = (max(src.shape[:2]), )*2 #initialize to opposite edges of image
     xmax, ymax = (0, )*2
 
     #make one big bounding box
     for c in ctr:
-        x, y, dx, dy = cv2.boundingRect(c) #candidate coords
+        x, y, dx, dy = cv2.boundingRect(c) #candidate bounds
         # ensure current contour meets thresh
-        if hypot(dx,dy) > thresh * hypot(*src.shape[:2]):
+        if hypot(dx, dy) > thresh * hypot(*src.shape[:2]):
             #resize bounding box if needed
-            xmin, ymin = min(x, xmin),min(y, ymin)
-            xmax, ymax = max(x+dx, xmax), max(y+dy, ymax)
+            xmin, ymin = min(x, xmin), min(y, ymin)
+            xmax, ymax = max(x + dx, xmax), max(y + dy, ymax)
 
     #find side length & new bottom left corner
-    len = max(xmax-xmin, ymax-ymin)
-    xmin -= (len - (xmax-xmin))//2
-    ymin -= (len - (ymax-ymin))//2
+    len = max(xmax - xmin, ymax - ymin)
+    xmin -= (len - (xmax - xmin)) // 2
+    ymin -= (len - (ymax - ymin)) // 2 # bring object to the center of the image
 
-    #use numpy slicing to crop src
+    #use numpy slicing to crop src, return result
     return(src[ymin:ymin+len, xmin:xmin+len])
+
 
 def hypot(a, b):
     """calculates hypotenuse of vectors a and b
