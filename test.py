20c48ee8fb5257c64361237ffde25eea1cf617d0
Antonio V. Leonti <aleonti@umassd.edu>
2020-01-29 17:48:07 -0500

    dista

--- test.py
+++ test.py
@@ -4,30 +4,23 @@
 
 import numpy
 import cv2
+from matplotlib import pyplot as plt
 
 def main():
     #import image
-    img = cv2.imread("data/batch_test.tif", cv2.IMREAD_GRAYSCALE)
-    kernel = numpy.ones((3,3),numpy.uint8) #for later
+    img = cv2.imread("data/train/amp_radiarta/amp_radiarta_100496.png", cv2.IMREAD_GRAYSCALE)
     #threshold
-    thr = cv2.threshold(img, 0,255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
-
-    opening = cv2.morphologyEx(thr, cv2.MORPH_OPEN, kernel, iterations = 2)
-    # sure background area
-    sure_bg = cv2.dilate(opening, kernel, iterations = 3)
+    thr = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
     # Finding sure foreground area
-    dist = cv2.distanceTransform(thr, cv2.DIST_L2, 5)
-    print(dist.max())
+    dist_transform = cv2.distanceTransform(thr, cv2.DIST_L2, 3)
     #sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)[1]
 
     show_image("thr", thr)
-    show_image("opening", opening)
-    show_image("sure_bg", sure_bg)
-    show_image("dist", dist)
+    show_image("dist", dist_transform)
     #show_image("sure_fg", sure_fg)
 
 def show_image(str, img):
-    cv2.imshow(str, img)
+    cv2.imshow(str,img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
