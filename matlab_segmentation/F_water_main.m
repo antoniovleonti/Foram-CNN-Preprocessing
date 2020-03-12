76b105203c282f6c4091de3720bc81914b4d3fd5
Antonio V. Leonti <aleonti@umassd.edu>
2020-03-11 22:46:00 -0400

    more comments

--- matlab_segmentation/F_water_main.m
+++ matlab_segmentation/F_water_main.m
@@ -1,8 +1,8 @@
 
 %%
-function water = F_water_main(water,job)
+function water = F_water_main(water, job)
 
-% wtf is cutlarge??
+% WHAT is cutlarge??
 if job.cutlarge && min(water.ridge(:)) == 1
     disp('first time!')
 end
@@ -10,97 +10,100 @@ end
 aratio = job.aratio;
 
 disp('labelling and calculating property')
-temp_0   = bwconncomp(water.segment); %connected components
+temp_0 = bwconncomp(water.segment); %connected components
 
-% From https://www.mathworks.com/help/images/ref/labelmatrix.html, on the return value of labelmatrix()
+% From https://www.mathworks.com/help/images/ref/labelmatrix.html, on the return value of labelmatrix():
 % "Label matrix of contiguous regions, returned as matrix of nonnegative integers. The pixels labeled 0 are the background. The pixels labeled 1 make up one object; the pixels labeled 2 make up a second object; and so on."
-% EXAMPLE: (0 is background, 1, is the first object, 2 is the second, etc.)
+% EXAMPLE: (0 is background, 1, is the "first" object, 2 is the second, etc.)
 % { 0 0 0 0 0 0 0 0 0 0 ;                  { 0 0 0 0 0 0 0 0 0 0 ;
 %   0 1 1 0 1 1 0 1 1 0 ;                    0 1 1 0 2 2 0 3 3 0 ;
 %   0 1 1 0 1 1 0 1 1 0 ; ->labelmatrix()->  0 1 1 0 2 2 0 3 3 0 ;
 %   0 0 0 0 0 0 0 0 0 0 }                    0 0 0 0 0 0 0 0 0 0 }
-label_0  = labelmatrix(temp_0);
+label_0 = labelmatrix(temp_0);
+% calculate area of each region
 label_0p = regionprops(temp_0,'area');
-areas    = [label_0p.Area];
+areas = [label_0p.Area];
+
 clear temp_0; clear label_0p;
 
 
 cri_size = job.minarea;
 clear temp;
 
-% t1,t2,and t3 are the dimensions of all the data
-[t1,t2,t3]=size(water.segment);
-% create array of 1s of size size(water.segment)
-ridge_1=true(t1,t2,t3);
+% t1, t2, and t3 are the dimensions of the data
+[t1, t2, t3] = size(water.segment);
+% create array of 1s the same size as the data
+ridge_1 = true(t1, t2, t3);
 
-% for each region... (indices start at 1 in matlab, not zero)
-for counter = 1:max(label_0(:)) % for var start:end
+% for each region... (indices start at 1 in matlab, not zero -_-)
+for counter = 1 : max(label_0(:)) % for var start:end
     % print the counter variable
     disp(counter);
 
     % if the area of the currently "selected" region
     if (areas(counter) >= cri_size)
-        % "x = list == val" ; x is a matrix of the same size as list that has 1 where the element is equal to val and a 0 evverywhere else
         % make a copy of label_0 where the currently selected region is '1' and everything else is 0
         particle = label_0 == counter;
 
-        % ind2sub takes linear indices and returns subscripts ...??
-        % find(x) finds all the nonzero elements in a matrix x ..????
+        %% ************** "CROP" DATA **************
+        % crop data to only contain cells necessary to represent the currently selected region
 
-        % ok I figured it out: This returns an array of subscripts, and those subscripts point to every single non-zero value in particle
-        % what does a non-zero value in particle mean? It means the that label_0 is equal to counter at that subscript.
+        % This returns an array of subscripts, and those subscripts point to every single data point representing the currently selected region
         % This is necessary because find() returns an array of linear indices
-        [xx, yy, zz] = ind2sub([t1,t2,t3], find(particle));
+        [xx, yy, zz] = ind2sub([t1, t2, t3], find(particle));
 
-        disp(['stats',double(counter),double(max(label_0(:))),double(areas(counter)),double(max(areas))]);
+        disp([double(counter), double(max(label_0(:))), double(areas(counter)), double(max(areas))]);
 
         % so now we get the range of integer values in every direction where something lies at this height (height = "counter")
-        xrange = min(xx):max(xx);  % 0 : x = { 0, 1, 2, ... , x-1, x }
-        yrange = min(yy):max(yy);
-        zrange = min(zz):max(zz);
+        xrange = min(xx) : max(xx);  % 0 : x = { 0, 1, 2, ... , x-1, x }
+        yrange = min(yy) : max(yy);
+        zrange = min(zz) : max(zz);
 
         % if the currently selected area > .75 * the greatest area
         if areas(counter) > (0.75 * max(areas))
             % just a print statement
-            disp([min(xx),max(xx),min(yy),max(yy),min(zz),max(zz)]);
+            disp([min(xx), max(xx), min(yy), max(yy), min(zz), max(zz)]);
         end
 
-        % NEW VARIABLE particle_local
-        % looks like this is taking a 3d partition of particle, or more descriptively, "cropping" particle to only contain non-zero values?
-        particle_local = particle(xrange,yrange,zrange);
+        % particle_local is the final, cropped version of the data
+        particle_local = particle(xrange, yrange, zrange);
         clear particle;
 
-        % "local" Euclidean Distance Map is a distance map of the inverted 3d partition of particle ??
+        %%
+
+        % "local" euclidean distance map is a distance map of the inverted 3d partition of particle ??
         EDM_local = bwdist(~particle_local);
         % we now modify this iedm using our adaptive function
-        EDM_local_1 = F_water_modifyedm_adaptive(-EDM_local,aratio);
+        EDM_local_1 = F_water_modifyedm_adaptive(-EDM_local, aratio);
         clear EDM_local;
 
         disp('watersheding');
         % using the stock watershed function here ??
+        % do these functions "just work" on 3d images???
         water_local = watershed(EDM_local_1);
         % put a 1 where particle_local == 1 and the watershed result == 0
         cutline = particle_local & (water_local == 0);
 
-        ridge_1(xrange,yrange,zrange) = min(ridge_1(xrange,yrange,zrange),~cutline);
+        ridge_1(xrange, yrange, zrange) = min(ridge_1(xrange, yrange, zrange), ~cutline);
 
-        if max(cutline(:)) == 0%remove this particle if it's not segmented this time
+        % remove this particle if it's not segmented this time
+        if max(cutline(:)) == 0
             label_0(label_0 == counter) = 0;
         end
-        clear water_local;clear cutline;
+        clear water_local; clear cutline;
     else
-        label_0(label_0==counter)=0;
+        label_0(label_0 == counter) = 0;
     end
 end
 
-if min(ridge_1)==1
-    water.end=true;
+if min(ridge_1) == 1
+    water.end = true;
 end
 
-if job.modi==2 %no iteration for the traditional method
-    water.end=true;
+if job.modi == 2 %no iteration for the traditional method
+    water.end = true;
 end
 
-water.ridge=water.ridge & ridge_1;
-water.segment=water.segment & (label_0 > 0);%not useful after iteration
-water.segment=water.segment & ridge_1;
+water.ridge = water.ridge & ridge_1;
+water.segment = water.segment & (label_0 > 0);%not useful after iteration
+water.segment = water.segment & ridge_1;
--- matlab_segmentation/F_water_modifyedm_adaptive.m
+++ matlab_segmentation/F_water_modifyedm_adaptive.m
@@ -1,20 +1,25 @@
 % EDM already has negative values
-function MEDM=F_water_modifyedm_adaptive(EDM,aratio)
+function MEDM = F_water_modifyedm_adaptive(EDM, aratio)
 % get the locations of all the minima (set minima to 1, everything else to 1)
 LM = imregionalmin(EDM);
 % get the actual values of minima (everything else is multiplied by 0)
+% ".*" is element-wise multiplication
 minima = EDM .* LM;
 
+% we made it negative just to later get the absolute value??? anyway:
+% get abs() of every value in minima, then make a sorted array of all unique values in that minima
 values = unique(abs(minima));
+% exclude the first value (the first value is the SMALLEST)
 depths = values(2:end);
+% transposing the matrix... why??? isn't it one dimensional??
 depths = depths';
-basins = numel(depths);
 
 MEDM = EDM;
 
-if basins >= 2%at least two catchments found;
-    temp1 = [depths(2:end),0];
-    temp2 = temp1 - depths;
+if numel(depths) >= 2 % at least two catchments found;
+
+    temp1 = [depths(2:end),0]; % ??? why is a 0 added to the end?
+    temp2 = temp1 - depths; % ???
     ind = find(temp2 == max(temp2));
 
     cri_depth = depths(ind(1));
