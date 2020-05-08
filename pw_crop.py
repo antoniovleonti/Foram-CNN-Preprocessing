199129ff9575e4ad88fb7bab51170f1024f78d54
Antonio V. Leonti <aleonti@umassd.edu>
2020-05-07 20:10:43 -0400

    fixed pw_crop
    now works even if original image is too small to fit final square crop

--- pw_crop.py
+++ pw_crop.py
@@ -6,17 +6,28 @@
 
 from os import listdir
 from math import sqrt
+from volume import show
 import cv2
 import numpy
 
+exts = ("jpg", "jpeg", "png", "tif")
 
-def main(): # preprocess a training & validation datset from a single source
+def main_segmented():
+    root = "private/data/segmented/1/"
+    for name in sorted(listdir(root)):
+        # after checking its extension...
+        if name.rpartition('.')[-1].lower() in exts:
+            # open & crop image
+            img = pw_crop(src = cv2.imread(root+name,0), thr = 0.1)
+            show(img)
+
+
+def main_test(): # preprocess a training & validation datset from a single source
     root = "data/"
     classes = ( # class names & thresholds
         ("amp_radiarta/", .01), ("glob_menardii/",.01), ("glob_ruber/", .05),
         ("nglob_dutertrei/",.05), ("tril_sacculifer/", .01)
     )
-    exts = ("jpg", "jpeg", "png", "tif")
     l = 64 # height and width
 
     # for each species
@@ -26,9 +37,10 @@ def main(): # preprocess a training & validation datset from a single source
             # after checking its extension...
             if name.rpartition('.')[-1].lower() in exts:
                 # determine output path (validation or training)
-                path = "train/" if name.rpartition('_')[-1][0]!='5' else "vali/"
+                path = "test/train/" if name.rpartition('_')[-1][0]!='5' else "test/vali/"
                 # open & crop image
                 img = pw_crop(src = cv2.imread(root+"src/"+c+name,0), thr = t)
+                show(img);
                 # resize & save
                 cv2.imwrite(
                     root + path + c + name,
@@ -52,19 +64,24 @@ def pw_crop(src, thr = 0):
     for c in ctr:
         x, y, dx, dy = cv2.boundingRect(c) #candidate bounds
         # ensure current contour meets thresh
-        if _hypot(dx, dy) > thr * _hypot(*src.shape[:2]):
+        if __hypot(dx, dy) > thr * __hypot(*src.shape[:2]):
             # resize bounding box if needed
             xmin, ymin = min(x, xmin), min(y, ymin)
             xmax, ymax = max(x + dx, xmax), max(y + dy, ymax)
 
     # find side length & new bottom left corner
     len = max(xmax - xmin, ymax - ymin)
-    xmin -= (len - (xmax - xmin)) // 2
-    ymin -= (len - (ymax - ymin)) // 2 # bring object to the center of the image
+
+    _xmin = (len - (xmax - xmin)) // 2
+    _ymin = (len - (ymax - ymin)) // 2 # bring object to the center of the image
+    _xmax = _xmin + xmax - xmin
+    _ymax = _ymin + ymax - ymin
 
     # use numpy slicing to crop src, return result
-    return(src[ymin : ymin+len, xmin : xmin+len])
+    crop = numpy.zeros((len,len), dtype=src.dtype)
+    crop[_ymin:_ymax,_xmin:_xmax] = src[ymin:ymax,xmin:xmax]
 
+    return(crop)
 
 def __hypot(a, b):
     """calculates hypotenuse of vectors a and b
@@ -73,4 +90,4 @@ def __hypot(a, b):
 
 
 if __name__ == "__main__":
-    main()
+    main_segmented()
